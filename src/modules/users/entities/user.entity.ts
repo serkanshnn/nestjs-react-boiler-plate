8b734f73445eb4a48b0b34a4bf65788177b61cd7
Ferhat Demir <edemirferhat@gmail.com>
2023-01-29 14:55:02 +0300

    revert models

--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -1,4 +1,5 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+import { Post } from '../../posts/entities/post.entity';
 
 @Entity()
 export class User {
@@ -13,4 +14,7 @@ export class User {
 
   @Column({ default: true })
   isActive: boolean;
+
+  @OneToMany((type) => Post, (post) => post.user)
+  posts: Post[];
 }
